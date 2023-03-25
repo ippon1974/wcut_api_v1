f7d4d9f634b543e7df9b78b10115a1b9808f7787
Vladimir <plategi@yandex.ru>
2023-03-25 14:21:21 +0300

    o

--- src/main/java/ru/wcut/api/repository/ArticleRepo.java
+++ src/main/java/ru/wcut/api/repository/ArticleRepo.java
@@ -12,19 +12,19 @@ public interface ArticleRepo extends JpaRepository<ArticleEntity, Long> {
     @Query(value = "SELECT * FROM article ORDER BY dt DESC", nativeQuery = true)
     public List<ArticleEntity> findAllByOrderByIdDesc();
 
-    @Query(value = "SELECT * FROM article WHERE title_id = '1' ORDER BY dt DESC", nativeQuery = true)
+    @Query(value = "SELECT * FROM article WHERE title_id = '1' AND is_published = 1 ORDER BY dt DESC", nativeQuery = true)
     public List<ArticleEntity> findAllMarket();
 
-    @Query(value = "SELECT * FROM article WHERE title_id = '2' ORDER BY dt DESC", nativeQuery = true)
+    @Query(value = "SELECT * FROM article WHERE title_id = '2' AND is_published = 1 ORDER BY dt DESC", nativeQuery = true)
     public List<ArticleEntity> findAllCompany();
 
-    @Query(value = "SELECT * FROM article WHERE title_id = '3' ORDER BY dt DESC", nativeQuery = true)
+    @Query(value = "SELECT * FROM article WHERE title_id = '3' AND is_published = 1 ORDER BY dt DESC", nativeQuery = true)
     public List<ArticleEntity> findAllShow();
 
-    @Query(value = "SELECT * FROM article WHERE title_id = '4' ORDER BY dt DESC", nativeQuery = true)
+    @Query(value = "SELECT * FROM article WHERE title_id = '4' AND is_published = 1 ORDER BY dt DESC", nativeQuery = true)
     public List<ArticleEntity> findAllItem();
 
-    @Query(value = "SELECT * FROM article WHERE title_id = '5' ORDER BY dt DESC", nativeQuery = true)
+    @Query(value = "SELECT * FROM article WHERE title_id = '5' AND is_published = 1 ORDER BY dt DESC", nativeQuery = true)
     public List<ArticleEntity> findAllVideo();
 
     @Query(value = "SELECT * FROM article WHERE is_published = 1 AND title_id = 1 ORDER BY id ASC LIMIT 2", nativeQuery = true)
